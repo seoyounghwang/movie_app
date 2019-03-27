aa486eaf1e29cbe6b72015a997771cde2ada3280
suhyoung <suhyoung1030@gmail.com>
2019-03-27 17:17:39 +0900

    Updates

---
+++ precache-manifest.b61df65f19c82d3b3410f7093897a126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "4a5d5a2b2c3377d0de3c",
+    "url": "/movie_app/static/js/main.ec1394d8.chunk.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/movie_app/static/js/2.28db7df0.chunk.js"
+  },
+  {
+    "revision": "4a5d5a2b2c3377d0de3c",
+    "url": "/movie_app/static/css/main.8031e4ab.chunk.css"
+  },
+  {
+    "revision": "9099642e4db3c8cf491bc85a645eb1dc",
+    "url": "/movie_app/index.html"
+  }
+];
